8fe0adbb4492e3bde510de79e1bec16a14c2e15a
Chimennadi <nnadichime04@gmail.com>
2024-05-27 06:31:09 +0100

    templates

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Monthly
+from .models import Month
 
 # Register your models here.
-admin.site.register(Monthly)
+admin.site.register(Month)
--- app/models.py
+++ app/models.py
@@ -3,27 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class Monthly(models.Model):
-
+class Month(models.Model):
     def __str__(self):
-        return (
-            self.january + "<br>"
-            + self.febuary + "<br>"
-            + self.march + "<br>"
-            + self.april + "<br>"
-            + self.may + "<br>"
-            + self.june + "<br>"
-        )
+        return self.name + self.value
 
-    january = models.CharField(max_length=100)
-    febuary = models.CharField(max_length=100)
-    march = models.CharField(max_length=100)
-    april = models.CharField(max_length=100)
-    may = models.CharField(max_length=100)
-    june = models.CharField(max_length=100)
-    july = models.CharField(max_length=100)
-    august = models.CharField(max_length=100)
-    september = models.CharField(max_length=100)
-    october = models.CharField(max_length=100)
-    november = models.CharField(max_length=100)
-    december = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    value = models.CharField(max_length=100)
